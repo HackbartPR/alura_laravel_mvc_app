48a5a549ba3181ddb5cd6c03184382f4458048bd
HackbartPR <cgharckbart@gmail.com>
2023-06-26 20:24:36 -0300

    Adicionando Repositories de Duas Camadas

--- app/Repositories/SeriesRepository.php
+++ app/Repositories/SeriesRepository.php
@@ -2,62 +2,13 @@
 
 namespace App\Repositories;
 
-use App\Models\Episode;
-use App\Models\Season;
 use App\Models\Serie;
-use Exception;
-use Illuminate\Support\Facades\DB;
 
-class SeriesRepository
+interface SeriesRepository
 {
-    public function store(array $payload): Serie
-    {
-        $series = DB::transaction(function () use ($payload) {
-            try{
-                $series = Serie::create($payload);
-
-                $seasonList = $this->createSeasonList($payload['seasons'], $series->id);
-                Season::insert($seasonList);
-
-                $episodesList = [];
-                foreach ($series->seasons as $season) {
-                    $episodesList = array_merge($episodesList, $this->createEpisodeList($payload['episodes'], $season->id));
-                }
-                Episode::insert($episodesList);
-
-                return to_route('series.index')
-                    ->with('message.success', "Série '{$series->name}' criada com sucesso!");
-            }catch(Exception $e) {
-                throw new Exception($e);
-            }
-        });
-
-        return $series;
-    }
-
-    private function createSeasonList(int $amount, int $serieId): array
-    {
-        $seasons = [];
-        for($i = 1; $i<= $amount; $i++) {
-            $seasons[] = [
-                'number' => $i,
-                'series_id' => $serieId
-            ];
-        }
-
-        return $seasons;
-    }
-
-    private function createEpisodeList(int $amount, int $seasonId): array
-    {
-        $episodes = [];
-        for($i = 1; $i<= $amount; $i++) {
-            $episodes[] = [
-                'number' => $i,
-                'season_id' => $seasonId
-            ];
-        }
-
-        return $episodes;
-    }
+    /**
+     * @param array ['SeriesName', 'SeasonsAmount', 'EpisodesAmount']
+     * @return Serie
+     */
+    public function store(array $payload): Serie;
 }
